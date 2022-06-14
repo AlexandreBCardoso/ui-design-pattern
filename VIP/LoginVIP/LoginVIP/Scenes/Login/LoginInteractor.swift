05b19e9cb697e463f83c6dcebf370049ed404303
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-06-13 22:31:53 -0300

    cria camada interactor

--- VIP/LoginVIP/LoginVIP/Scenes/Login/LoginInteractor.swift
+++ VIP/LoginVIP/LoginVIP/Scenes/Login/LoginInteractor.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+protocol LoginBusinessLogic {
+    func validateData(request: LoginUseCase.Validate.Request)
+}
+
+protocol LoginDataStore {
+    var name: String? { get set }
+}
+
+class LoginInteractor: LoginDataStore {
+    var name: String?
+    
+    private let presenter: LoginPresentationLogic
+    
+    init(presenter: LoginPresentationLogic) {
+        self.presenter = presenter
+    }
+    
+    
+}
+
+extension LoginInteractor: LoginBusinessLogic {
+    
+    func validateData(request: LoginUseCase.Validate.Request) {
+        print(#function)
+        
+        // Verifica se dados são optional
+        guard let name = request.name,
+              let email = request.email
+        else {
+            return presenter.presentError()
+        }
+        
+        // Verifica se tem valor
+        if name.isEmpty || email.isEmpty {
+            presenter.presentError()
+        } else {
+            self.name = name
+            presenter.presentSuccess()
+        }
+    }
+    
+}
