09769111bff19fd6df786554069d2f6550b6e69e
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-06-13 22:21:46 -0300

    project initial

---
+++ VIP/LoginVIP/LoginVIP/Scenes/Login/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  LoginVIP
+//
+//  Created by Alexandre Cardoso on 13/06/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
