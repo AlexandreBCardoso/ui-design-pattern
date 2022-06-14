49cdd4505863da019312544834d5cd219d37f89e
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-06-13 22:31:20 -0300

    cria camada router

--- VIP/LoginVIP/LoginVIP/Scenes/Login/LoginRouter.swift
+++ VIP/LoginVIP/LoginVIP/Scenes/Login/LoginRouter.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+protocol LoginRoutingLogic {
+    func routerToHome()
+}
+
+protocol LoginDataPassing {
+    var dataStore: LoginDataStore? { get set }
+}
+
+typealias LoginRouterType = LoginRoutingLogic & LoginDataPassing
+
+class LoginRouter: LoginDataPassing {
+    var dataStore: LoginDataStore?
+    
+}
+
+extension LoginRouter: LoginRoutingLogic {
+    
+    func routerToHome() {
+        // Passar o Nome digitado para a Home
+        // Chamar a Home
+    }
+    
+}
