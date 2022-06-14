b2d717e7446472cce43708f40e16b1ec975c5fa5
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-06-13 22:30:56 -0300

    cria camada UseCase

--- VIP/LoginVIP/LoginVIP/Scenes/Login/LoginUseCase.swift
+++ VIP/LoginVIP/LoginVIP/Scenes/Login/LoginUseCase.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+enum LoginUseCase {
+    
+    enum Validate {
+        struct Request {
+            let name: String?
+            let email: String?
+        }
+    }
+    
+    enum Error {
+        struct Response {
+            let error: Swift.Error
+        }
+    }
+    
+}
